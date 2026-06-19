Fix PID controller initialization and improve feedback velocity calculation

HardwareInTheLoop/PID_tune.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 class PID:
-    def __init__(self,k_p,k_i,k_d,error_history,dt=0.1):
+    def __init__(self,k_p,k_i,k_d,error_history=None,dt=0.1):
         self.k_p = k_p
         self.k_i = k_i
         self.k_d = k_d
@@ -13,22 +13,22 @@ class PID:
         error= x_d - pelvis
         self.error_history.append(error)
 
-        if len(self.error_history) > 10:
-            i_term= self.k_i * np.trapz(self.error_history, np.arange(len(self.error_history)))
+        i_term= self.k_i * np.trapz(self.error_history, np.arange(len(self.error_history)))
+        p_term = self.k_p * error 
+        if len(self.error_history) >= 2:
             d_term = ((error - self.error_history[-2]) / self.dt) * self.k_d
-            p_term = self.k_d * error 
-            feedback_velocity = d_term + i_term + p_term
-            return feedback_velocity
         else:
-            return None
+            d_term=0
 
+        feedback_velocity = d_term + i_term + p_term
+        return feedback_velocity
 
 dt=0.1
-controller = PID(k_p=1.5,k_i=0.5,k_d=0.1,dt=dt)
+controller = PID(k_p=1.5,k_i=0.5,k_d=0.3,dt=dt)
 
 x_d = -0.3
 pelvis=-1.2
-length=100
+length=1000
 list=[]
 time_history=[]
 